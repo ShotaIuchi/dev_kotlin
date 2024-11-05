e1108628a4a17f6420cff222f55364cc17a0c092
shota iuchi <shotaiuchi.develop@gmail.com>
2024-11-05 22:58:15 +0900

    exec selector

--- 2.cmd-exec.sh
+++ 2.cmd-exec.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-docker container exec -it dev_kotlin_springboot /bin/bash
+# Get names of running Docker containers
+containers=($(docker ps --format "{{.Names}}"))
+
+# Check if there are no running containers
+if [ ${#containers[@]} -eq 0 ]; then
+    echo "No running containers found"
+    exit 1
+fi
+
+# Display selection menu
+echo "Which container would you like to attach to?"
+PS3="Please select an option : "
+
+select container in "${containers[@]}"; do
+    if [[ -n "$container" ]]; then
+        docker container exec -it $container /bin/bash
+        break
+    else
+        echo "Invalid selection. Please try again."
+    fi
+done
