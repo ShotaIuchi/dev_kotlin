51ba0a21b9e46515b181bb3acd71705926c237ca
ShotaIuchi <shotaiuchi.develop@gmail.com>
2024-12-04 23:00:05 +0900

    Rename database service to mysql in config files

--- mysql/initialize.sh
+++ mysql/initialize.sh
@@ -1,7 +1,7 @@
-mkdir -p ./database/data
-mkdir -p ./database/init
+mkdir -p ./mysql/data
+mkdir -p ./mysql/init
 
-export DATABASE_HOST_PORT=3306
+export MYSQL_HOST_PORT=3306
 
 export MYSQL_ROOT_PASSWORD="root"
 export MYSQL_DATABASE="dev_space"
